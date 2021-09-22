a22d46ec4d55a44d5f8ff30c5400ed3e0c5fd581
Jaime Alvarez <jrralvarez@gmail.com>
2021-09-22 15:25:02 -0400

    added a route to get all the users.

---
+++ routes/users.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+//get all users
+router.get("/", async(req,res) => {
+    try {
+        const users = await User.find();
+        return res.send(users);
+    }   catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+module.exports = router;
