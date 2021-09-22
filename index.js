266d81d8a717920d97ea4602e360ebebe51f3011
Jaime Alvarez <jrralvarez@gmail.com>
2021-09-22 14:33:49 -0400

    able to connect to the mongoDB for petFinder.

---
+++ index.js
@@ -0,0 +1,16 @@
+const connectDB = require("./startup/db");
+const express = require("express");
+const app = express();
+//const users = require("./routes/users");
+const cors = require("cors");
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+//app.use("/api/users", users);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server started on port: ${port}`);
+});
