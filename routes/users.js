4e0f033789f70fb730d8f2a573b53b48cb576bc0
Jaime Alvarez <jrralvarez@gmail.com>
2021-09-22 16:18:10 -0400

    able to get a specific user

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const {User} = require("../models/user");
 
 //get all users
 router.get("/", async(req,res) => {
     try {
         const users = await User.find();
         return res.send(users);
+    }   catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+//get specific user
+router.get("/:id", async(req,res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        return res.send(user);
     }   catch (ex) {
         return res.status(500).send(`Internal Server Error: ${ex}`);
     }
