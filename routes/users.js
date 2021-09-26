030c187965c889ecc0272a0dfc936b942847dc24
Jaime Alvarez <jrralvarez@gmail.com>
2021-09-25 20:52:09 -0500

    added a route to add a new user.

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const {User} = require("../models/user");
+const bcrypt = require("bcrypt");
+
 
 //get all users
 router.get("/", async(req,res) => {
@@ -21,5 +23,38 @@ router.get("/:id", async(req,res) => {
         return res.status(500).send(`Internal Server Error: ${ex}`);
     }
 });
+
+//register new user
+router.post("/", async (req, res) => {
+    try {
+        const {error} = validateUser(req.body);
+        if (error) return res.status(400).send(error.details[0].message);
+        
+        let user = await User.findOne({email: req.body.email});
+        if (user) return res.status(400).send("User already registered.");
+
+        const salt = await bcrypt.genSalt(10);
+  
+        user = new User({
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          email: req.body.email,
+          password: await bcrypt.hash(req.body.password, salt),
+        });
+    
+        await user.save();
+    
+        const { password, ...sendUser } = user._doc;
+    
+        const token = user.generateAuthToken();
+    
+        return res
+          .header("x-auth-token", token)
+          .header("access-control-expose-headers", "x-auth-token")
+          .send(sendUser);
+      } catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+})
 
 module.exports = router;
